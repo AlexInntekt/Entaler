4947d6c4ad11ff4ccd24f4ae3c571cca29090e4e
Mihai Alexandru Manolescu <alex.inntekt@gmail.com>
2019-04-11 15:52:54 +0300

    sp

--- src/EntalerProject/urls.py
+++ src/EntalerProject/urls.py
@@ -19,12 +19,13 @@ from django.conf.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('team/', include('employees.urls')),
+    path('test/', TemplateView.as_view(template_name='test.html'))
 ]
 
 
