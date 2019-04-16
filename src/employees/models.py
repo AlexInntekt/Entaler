7cee0c557fd68b62d5397a5b8c54dbff0e76168b
Alexandru-Mihai Manolescu <alex.inntekt@gmail.com>
2019-04-16 10:38:39 +0300

    save progress

--- src/employees/models.py
+++ src/employees/models.py
@@ -4,6 +4,7 @@ class Employee(models.Model):
     name = models.CharField(max_length=35)
     about = models.TextField(default='')
     age = models.IntegerField()
+    image = models.ImageField(upload_to='postImage', blank=True)
 
     def __str__(self):
         return self.name
--- src/employees/views.py
+++ src/employees/views.py
@@ -1,6 +1,11 @@
+"""
+employees/views
+"""
 from django.shortcuts import render
 from django.views.generic.list import ListView
+
 from .models import Employee
+
 
 class EmployeesListView(ListView):
 	model = Employee
