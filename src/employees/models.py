a4072225c78e9a6589043543128950a29912dc97
Alexandru-Mihai Manolescu <alex.inntekt@gmail.com>
2019-04-16 10:20:09 +0300

    added field in employee model

--- src/employees/models.py
+++ src/employees/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Employee(models.Model):
     name = models.CharField(max_length=35)
+    about = models.TextField(default='')
     age = models.IntegerField()
 
     def __str__(self):
--- src/employees/urls.py
+++ src/employees/urls.py
@@ -4,4 +4,5 @@ from .views import EmployeesListView
 
 urlpatterns = [
 	path('', EmployeesListView.as_view()),
+
 ]
